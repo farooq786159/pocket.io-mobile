4b45a4ec3faa0ca186cbfbaf72a82960c16d0a26
iamarooq <farooqahmed21001@gmail.com>
2023-05-17 12:51:32 +0500

    layout of home and about pages

---
+++ src/components/About/AboutSecondaryPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+import SecondaryArticle from "./AboutSecondaryArticle.vue"
+</script>
+
+<template>
+    <main class="  w-full pl-20 pr-20 bg-gray-800">
+        <div class=" text-white  ">
+            <SecondaryArticle/>         
+        </div>
+    </main>
+</template>
+
