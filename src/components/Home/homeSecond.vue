10d8d374489ed71f0ea824100b7ee702c228e6f3
iamarooq <farooqahmed21001@gmail.com>
2023-05-28 18:13:19 +0500

    change in landingHome to secondHome

--- src/components/Home/homeSecond.vue
+++ src/components/Home/homeSecond.vue
@@ -1,5 +1,39 @@
 <template>
+    <main class="ml-8 mr-8 h-full">
+        <div class=" text-4xl font-bold  mt-14 mb-5 ">
+            Technology leaders through cutting-edge solutions.
+        </div>
+        <p class=" mb-20">30+ years empowering business</p>
 
+        <div class="flex justify-between items-center  h-60 mx-auto  p-0 mb-8  ">
+            <p class="  mt-32  mb-5 bg-black w-[20px] rounded-full h-[70px] p-0 ">
+                <span class="text-white rotate-90 ">
+                    <img src="../new_incons/svgexport-18.png" class="ml-1 mt-2 p-0  h-12"  alt="no img"> 
+                </span>
+            </p>             
+        </div>
+        <div class="flex  justify-between" >
+            <p class="text-xs">How we think</p>
+            <img src="../new_incons/svgexport-21.png" alt="no img" class="h-10 mt-3">
+        </div>
+        <div class=" mt-5">
+            <p class=" text-4xl font-bold ml-5 mr-5">
+                We're challlengers at heart
+            </p>
+            <p class="ml-5 mr-5 mt-14 text-slate-400  font-bold text-lg">
+                PocketAd is a collective brain,
+                formed by a diverse group of
+                people who bring years of
+                experience, dedication and
+                everlasting devotion to 
+                digital craftsmanship.
+            </p>
+        </div>
+        <div  class="w-[300px]  inline-flex  mt-10  mb-24   ">                
+                <p class="rounded-full py-2 px-5 text-center  text-gray-900 text-xs font-extrabold  " style="background:#ffff05;">Dive into our culture</p>     
+                <img src="../new_incons/svgexport-20.png" alt="" class="rounded-full  p-3 w-10" style="background:#ffff05;">    
+        </div>
+    </main>
     <main class="  relative bg-gradient-to-br from-inherit text-white ">
         <img src="../icons/pocketad.io_home_image1.png" class=" absolute h-full object-cover mix-blend-overlay "/>
         <div class="w-full h-full " >
