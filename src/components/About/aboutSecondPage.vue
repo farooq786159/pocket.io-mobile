161b0f4fb96a47237b9ba0f41db63817ce829e36
iamarooq <farooqahmed21001@gmail.com>
2023-06-02 00:36:44 +0500

    change in button for about leadership

--- src/components/About/aboutSecondPage.vue
+++ src/components/About/aboutSecondPage.vue
@@ -22,35 +22,49 @@
                 <p class="mt-10 pl-2 pr-2">Before anything else, PocketAd is an ecosystem. We have always had clarity about the fact that we are a part of something bigger, and that we nurture from it every bit as much as it is our responsibility to take good care of our surroundings. The only way to be consistent in our effort to co-create the future, is to also create the conditions for that future to be a better version of today.</p>
             </div>
 
-            <div class="mt-10 mb-10" >
-                <p class=" text-[78px] font-bold leading-[5rem]">leadership <span  class="block pl-44 ">team </span> </p>
-                <p class="mt-10 px-2">We organize our work around practices, and each practice is led by skilled mentors that bring all of their experience and proficiency to their team.These are the referents that lead the way in our everyday craft.</p>
-            </div>
-            <div class="slider">
-                <div class='slider-content' :style="sliderStyle">
-                   <div class="slider-item"
-                            v-for="(item, index) in items" :key="index"
-                            :class="{ active: currentIndex === index }">
-                    <p class="text-4xl font-bold  ">{{item.title}}</p>
-                    <P class="mb-5 mt-5 px-1">{{item.description}}</P>
-                   </div>
-
-                </div>
-                <div class="flex justify-center  pb-10 ">
-                    <button class='rounded-full bg-white  p-5 rotate-180 left-button' @click="slideLeft">
-                        <img src="../new_incons/svgexport-20.png" alt="" class="" >   
-                    </button>
-                    <p class=" text-slate-50 ml-9 mt-5 mr-9 text-lg">{{currentIndex+1}}/4</p>
-                    <button class='rounded-full bg-white  p-5 right-button ' @click="slideRight">
-                        <img src="../new_incons/svgexport-20.png" alt="" class="" >   
-                    </button>
-                </div>
-            </div>
+            
             
 
         </div>
         
     </div>
+
+    <div class='' >
+        <div class="mt-10 " >
+                <p class=" text-[78px] font-bold leading-[5rem]">leadership <span  class="block pl-44 ">team </span> </p>
+                <p class="mt-10 px-2 mb-20">We organize our work around practices, and each practice is led by skilled mentors that bring all of their experience and proficiency to their team.These are the referents that lead the way in our everyday craft.</p>
+            </div>
+            <div class='grid grid-cols-11 '>
+              <div class=' col-span-1  bg-slate-300' @click="slideLeft">
+                <img src="../new_incons/svgexport-43.svg" alt="" class='ml-1  rotate-180  h-5 mt-72'>
+              </div>
+              <div class="slider col-span-9">
+                <div class='slider-content' :style="sliderStyle">
+                   <div class="slider-item"
+                            v-for="(item, index) in items" :key="index"
+                            :class="{ active: currentIndex === index }">
+                    <p class="text-4xl font-bold text-center  ">{{item.title}}</p>
+                    <P class=" mt-5 px-1 ">{{item.description}}</P>
+                    
+                   </div>
+
+                </div>
+                <div class="flex justify-center  pb-10 ">
+                    <button class='rounded-full bg-white  p-5 rotate-180 left-button' @click="slideLeft">
+                        <img src="../new_incons/svgexport-20.svg" alt="" class="h-5" >   
+                    </button>
+                    <p class=" text-slate-500 ml-9 mt-5 mr-9  text-4xl">{{currentIndex+1}}/4</p>
+                    <button class='rounded-full bg-white  p-5 rotate-35  right-button ' @click="slideRight">
+                        <img src="../new_incons/svgexport-20.svg" alt="" class="h-5" >   
+                    </button>
+                </div>
+            </div>
+            <div class=' col-span-1  bg-slate-300 ' @click="slideRight">
+              <img src="../new_incons/svgexport-43.svg" alt="" class='ml-2  h-5 mt-72'>
+            </div>
+            </div>
+    </div>
+
     <div class="mr-2 ml-2 pb-20" >
         <div class="pt-24 flex justify-between mb-5  ">
             <p class=" text-lg  font-medium ">Services</p>
